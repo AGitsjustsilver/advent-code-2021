7281422e1ac5ab566b8d99d591d1a3bea7091533
Alessandro Guaresti <alessguar@gmail.com>
2022-01-21 07:39:18 -0800

    save

---
+++ Day1/cmake-build-debug/CMakeFiles/advent_code_2021.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/advent_code_2021.dir/main.c.o"
+  "advent_code_2021"
+  "advent_code_2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/advent_code_2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
